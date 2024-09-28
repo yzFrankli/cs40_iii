7af38acb34a32ce15e0f1c266f9d54e0317df56b
SiddNan <88634893+SiddNan@users.noreply.github.com>
2024-09-28 15:47:59 -0400

    Update bit2.h

--- bit2.h
+++ bit2.h
@@ -4,29 +4,27 @@
  *     9/24/2024
  *     CS40 HW2: iii
  *
- *     
  */
-
 
 #ifndef _BIT2_H_
 #define _BIT2_H_
 
 #include <stdio.h>
-#include "bit.h"
+#include <stdlib.h>
+#include <assert.h>
+#include <bit.h>
 
-struct Bit2 {
+// Define the Bit2 structure
+typedef struct Bit2 {
     int rows;
     int columns;
     int size;
-    Bit_T Bit_Array;
-};
+    Bit_T Bit_Array;  // Assuming Bit_T is used to store bits
+} Bit2;
 
-
-
-struct Bit2 *create_Bit2(int rows, int columns, int element_size);
+// Function declarations
+Bit2 *create_Bit2(int rows, int columns, int element_size);
 void Bit2_map_col_major(Bit2 *Bit_2, void (*apply)(void *element));
-void UArray2_map_row_major(Bit2 *Bit_2, void (*apply)(void *element));
+void Bit2_map_row_major(Bit2 *Bit_2, void (*apply)(void *element));
 
-
-
-#endif 
+#endif /* _BIT2_H_ */
