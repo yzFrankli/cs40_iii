8b85ea6135ca06e6b16a52e049888a4d509d2e88
FrankL <55543424+yzFrankli@users.noreply.github.com>
2024-09-28 15:17:31 -0400

    Update uarray2.h

--- uarray2.h
+++ uarray2.h
@@ -4,24 +4,24 @@
  *     9/24/2024
  *     CS40 HW2: iii
  *
- *     
  */
+
 #ifndef _UARRAY2_T_
 #define _UARRAY2_T_
 
-
 #include <uarray.h>
 
-struct UArray2 {
+// Use typedef to avoid having to use 'struct' every time
+typedef struct UArray2 {
     int rows;
     int columns;
     int size;
     UArray_T UArray;
-};
+} UArray2;  // This creates an alias 'UArray2' for 'struct UArray2'
 
-struct UArray2 *create_Uarray2(int rows, int columns, int element_size);
-void UArray2_map_col_major(UArray_T *U_Array2, void (*apply)(void *element));
-void UArray2_map_row_major(UArray_T *U_Array2, void (*apply)(void *element));
-
+// Function declarations
+UArray2 *create_Uarray2(int rows, int columns, int element_size);
+void UArray2_map_col_major(UArray2 *U_Array2, void (*apply)(void *element));  // Changed to use UArray2 alias
+void UArray2_map_row_major(UArray2 *U_Array2, void (*apply)(void *element));  // Changed to use UArray2 alias
 
 #endif /* _UARRAY2_T_ */
