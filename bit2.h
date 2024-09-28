3c71965bd8b427bc7671ed5d913607b7116cb0d4
SiddNan <88634893+SiddNan@users.noreply.github.com>
2024-09-28 17:51:02 -0400

    Update bit2.h

--- bit2.h
+++ bit2.h
@@ -11,14 +11,15 @@
 
 typedef struct Bit2_T *Bit2_T;  // Typedef for the Bit2_T type
 
-// Function declarations (replace with the actual functions you have)
+// Function declarations
 Bit2_T Bit2_new(int width, int height);
 int Bit2_width(Bit2_T array);
 int Bit2_height(Bit2_T array);
 int Bit2_get(Bit2_T array, int row, int col);
 int Bit2_put(Bit2_T array, int row, int col, int value);
-void Bit2_map_col_major(Bit2_T array, void apply(int i, int j, Bit2_T a, int b, void *p1), void *cl);
-void Bit2_map_row_major(Bit2_T array, void apply(int i, int j, Bit2_T a, int b, void *p1), void *cl);
 void Bit2_free(Bit2_T *array);
+void Bit2_map_row_major(Bit2_T array, void apply(int i, int j, Bit2_T a, int value, void *p1), void *p1);
+void Bit2_map_col_major(Bit2_T array, void apply(int i, int j, Bit2_T a, int value, void *p1), void *p1);
 
 #endif /* BIT2_INCLUDED */
+
