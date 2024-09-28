14dd78193bfc75472b02d29ccd8e037a3269b7e3
SiddNan <88634893+SiddNan@users.noreply.github.com>
2024-09-28 16:31:54 -0400

    Update bit2.h

--- bit2.h
+++ bit2.h
@@ -6,25 +6,22 @@
  *
  */
 
-#ifndef _BIT2_H_
-#define _BIT2_H_
+#ifndef BIT2_H
+#define BIT2_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <assert.h>
-#include <bit.h>
+#include <uarray.h>  // Include necessary libraries
 
-// Define the Bit2 structure
+// Define the Bit2 structure only once
 typedef struct Bit2 {
     int rows;
     int columns;
-    int size;
-    Bit_T Bit_Array;  // Assuming Bit_T is used to store bits
+    UArray_T Bit_Array;  // Use Hanson's UArray to store the bits
 } Bit2;
 
 // Function declarations
 Bit2 *create_Bit2(int rows, int columns, int element_size);
-void Bit2_map_col_major(Bit2 *Bit_2, void (*apply)(void *element));
-void Bit2_map_row_major(Bit2 *Bit_2, void (*apply)(void *element));
+void Bit2_map_row_major(Bit2 *bit2, void (*apply)(void *element));
+void Bit_set(UArray_T array, int index, int value);
+int Bit_get(UArray_T array, int index);
 
-#endif /* _BIT2_H_ */
+#endif /* BIT2_H */
